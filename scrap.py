c1e2bb99754b48244812c65bbb5f8ae2fdff5308
Melvin Chia <64565584+melvinchia3636@users.noreply.github.com>
2020-08-26 06:09:28 +0800

    Added a function container
    Added a function container to the code of books details scraping

--- scrap.py
+++ scrap.py
@@ -13,18 +13,19 @@ def scrap_catalogue(csv_file):
     dataframe = pd.DataFrame(data, columns =['book name', 'price', 'rating', 'instock availability']) 
     dataframe.to_csv(csv_file, index=False, header=True)
 
-data = []
+def scrap_details(csv_file):
+    data = []
 
-for i in range(1, 51): 
-    raw = BeautifulSoup(requests.get('http://books.toscrape.com/catalogue/page-'+str(i)+'.html').content, 'html.parser').find('ol', {'class', 'row'}).findAll('li')
-    for k in ['http://books.toscrape.com/catalogue/'+j.find('a')['href'] for j in raw]:
-        d_raw = BeautifulSoup(requests.get(k).content, 'html.parser').find('div', {'class': 'content'})
-        title = d_raw.find('h1').text
-        rating = w2n.word_to_num(re.search('<p class=\"star-rating \w+\">', str(d_raw)).group().replace('<p class=\"star-rating ', '').replace('\">', ''))
-        UPC, product_type, price_excl_tax, price_incl_tax, tax, availability, number_of_reviews = [l.find('td').text for l in  d_raw.find('table', {'class': 'table table-striped'}).findAll('tr')]
-        data.append([title, rating, UPC, product_type, price_excl_tax, price_incl_tax, tax, availability, number_of_reviews])
-        print(title)
-    print(i, ' / 50')
+    for i in range(1, 51): 
+        raw = BeautifulSoup(requests.get('http://books.toscrape.com/catalogue/page-'+str(i)+'.html').content, 'html.parser').find('ol', {'class', 'row'}).findAll('li')
+        for k in ['http://books.toscrape.com/catalogue/'+j.find('a')['href'] for j in raw]:
+            d_raw = BeautifulSoup(requests.get(k).content, 'html.parser').find('div', {'class': 'content'})
+            title = d_raw.find('h1').text
+            rating = w2n.word_to_num(re.search('<p class=\"star-rating \w+\">', str(d_raw)).group().replace('<p class=\"star-rating ', '').replace('\">', ''))
+            UPC, product_type, price_excl_tax, price_incl_tax, tax, availability, number_of_reviews = [l.find('td').text for l in  d_raw.find('table', {'class': 'table table-striped'}).findAll('tr')]
+            data.append([title, rating, UPC, product_type, price_excl_tax, price_incl_tax, tax, availability, number_of_reviews])
+            print(title)
+        print(i, ' / 50')
 
-dataframe = pd.DataFrame(data, columns =['book name', 'rating', 'UPC', 'product type', 'price exclude tax', 'price include tax', 'tax', 'availability', 'number of reviews']) 
-dataframe.to_csv('book_details.csv', index=False, header=True)
+    dataframe = pd.DataFrame(data, columns =['book name', 'rating', 'UPC', 'product type', 'price exclude tax', 'price include tax', 'tax', 'availability', 'number of reviews']) 
+    dataframe.to_csv(csv_file, index=False, header=True)
